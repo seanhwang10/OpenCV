9eee81ec138f59961648eea11b5ca7f54ee72b46
Sean Hwang <hwang103@purdue.edu>
2020-12-25 03:33:17 -0500

    c8 begin

--- c7 color detection.py
+++ c7 color detection.py
@@ -4,6 +4,39 @@ import numpy as np
 def empty(a):
     pass
 
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+
 cv2.namedWindow("Trackbars")
 cv2.resizeWindow("Trackbars",640,240)
 cv2.createTrackbar("Hue Min", "Trackbars", 105, 179, empty)
@@ -29,8 +62,9 @@ while True:
     mask = cv2.inRange(imgHSV, lower, upper)
     imgFinal = cv2.bitwise_and(img,img,mask=mask)
 
-    cv2.imshow("original", img)
-    cv2.imshow("HSV", imgHSV)
-    cv2.imshow("Mask", mask)
-    cv2.imshow("Final Image", imgFinal)
+
+
+    imgStack = stackImages(0.7,([img,imgHSV],[mask,imgFinal]))
+    cv2.imshow("original", imgStack)
+
     cv2.waitKey(1)
